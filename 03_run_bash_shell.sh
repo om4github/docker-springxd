255d4da9b33d2366724561eb258c2e3ef4d435fb
David Medinets <david.medinets@gmail.com>
2014-07-06 00:39:29 +0000

    Add script to run bash shell

---
+++ 03_run_bash_shell.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IMAGE_ID=$(docker ps -a | grep "springxd-bash" | cut -f1 -d' ')
+[ -z "$IMAGE_ID" ] || docker stop $IMAGE_ID | xargs docker rm | echo "Removed $IMAGE_ID"
+
+IP=`echo $(ifconfig eth0 | awk -F: '/inet addr:/ {print $2}' | awk '{ print $1 }')`
+
+URL="http://$IP:9393/"
+
+echo "Waiting for Spring XD Single Node to respond."
+until $(curl --output /dev/null --silent --head --fail $URL); do
+    printf '.'
+    sleep 5
+done
+
+echo ""
+echo "HOST IP: $IP"
+echo ""
+docker run --name=springxd-bash -i -t $USER/springxd:1.0.0.M7 /bin/bash
